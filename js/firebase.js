1b8c0223f715f4ae25738d99ec2d58ebceb1f00c
SeanSoliman25 <161667439+SeanSoliman25@users.noreply.github.com>
2026-03-14 10:54:49 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase
 
 // Your exact web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD30sheL6HR5q95MxMRvqVWT_9ON3ML6uk",
-    authDomain: "commu-nect.firebaseapp.com",
-    projectId: "commu-nect",
-    storageBucket: "commu-nect.firebasestorage.app",
-    messagingSenderId: "67056180570",
-    appId: "1:67056180570:web:af9fbad96687feecd82763"
+    apiKey: "AIzaSyDX7NmAsDkBik-mbmEWqwodLUv9nQjJ65g",
+    authDomain: "commu-nect-e6bb9.firebaseapp.com",
+    projectId: "commu-nect-e6bb9",
+    storageBucket: "commu-nect-e6bb9.firebasestorage.app",
+    messagingSenderId: "589689646614",
+    appId: "1:589689646614:web:ac474ff850d276a263cf37"
 };
 
 // Initialize Firebase
